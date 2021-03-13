57e96421f8f7fcfc2b91c17049c16bafd125ded2
nemesis-lab <53715853+nemesis-lab@users.noreply.github.com>
2021-03-13 10:05:46 -0300

    Extrai e organiza os dados

--- Extrair_dados_Argentina.R
+++ Extrair_dados_Argentina.R
@@ -7,8 +7,10 @@ library(readr)
 argentina <- function(df_choice){
     
   y <- as.character(df_choice)
+  
+  # ABRE O ARQUIVO DO BANCO
 
-  file_name <- paste("C:/Users/Carlos Garcia Filho/Desktop/Argentina/DefWeb",as.character(y),".csv", sep = "") 
+  file_name <- paste("C:/Users/Carlos Garcia Filho/Desktop/Comparativo - Brasil, Argentina e Colômbia/Argentina/DefWeb",as.character(y),".csv", sep = "") 
   
   # DROP COLUNAS QUE NÃO SERÃO UTILIZADAS
   
@@ -26,35 +28,21 @@ argentina <- function(df_choice){
 
   # FILTRA AS CAUSAS DE INTERESSE
   
-  df <- df %>% filter(CAUSA == "X60" | 
-                      CAUSA == "X61" |
-                      CAUSA == "X62" |
-                      CAUSA == "X63" |
-                      CAUSA == "X64" |
-                      CAUSA == "X65" |
-                      CAUSA == "X66" |
-                      CAUSA == "X67" |
-                      CAUSA == "X68" |
-                      CAUSA == "X69" |
-                      CAUSA == "X70" |
-                      CAUSA == "X71" |
-                      CAUSA == "X72" |
-                      CAUSA == "X73" |
-                      CAUSA == "X74" |
-                      CAUSA == "X75" |
-                      CAUSA == "X76" |
-                      CAUSA == "X77" |
-                      CAUSA == "X78" |
-                      CAUSA == "X79" |
-                      CAUSA == "X80" |
-                      CAUSA == "X81" |
-                      CAUSA == "X82" |
-                      CAUSA == "X83" |
-                      CAUSA == "X84")
-
+  df <- df %>% filter(str_detect(CAUSA, "^X6|^X7|^X81|^X82|^X83|^X84"))
+  
+  # AGRUPA OS RESULTADOS POR SEXO E CAUSA
+  
+  df <- df %>% group_by(CAUSA, SEXO) %>%
+    summarise(QT = sum(CUENTA))
+  
+  # CRIA AS COLUNAS ANO E PAÍS
+  
+  df$ANO = y
+  df$PAIS = "Argentina"
+  
   # SALVA O ARQUIVO EM CSV
   
-  file_name_final <- paste("C:/Users/Carlos Garcia Filho/Desktop/Argentina/",as.character(y),"final.csv", collapse = NULL) 
+  file_name_final <- paste("C:/Users/Carlos Garcia Filho/Desktop/Comparativo - Brasil, Argentina e Colômbia/Argentina/20",as.character(y),"_AR_final.csv", sep = "") 
   write.csv(df, file_name_final, row.names = FALSE)
   
 }
@@ -62,3 +50,5 @@ argentina <- function(df_choice){
 periodo <- c("09", "10", "11", "12", "13", "14", "15", "16", "17", "18")
 
 sapply(periodo, argentina)
+
+# AMDG
